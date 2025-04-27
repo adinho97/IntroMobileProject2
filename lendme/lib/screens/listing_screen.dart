205a886d023fd0aa0530020a17ee53280c7fe0a5
adinho97 <adrian.dys97@gmail.com>
2025-04-27 21:44:47 +0200

    Add state with list, on click add using dummy data

--- lendme/lib/screens/listing_screen.dart
+++ lendme/lib/screens/listing_screen.dart
@@ -6,3 +6,44 @@ class ListingScreen extends StatefulWidget {
     @override
     State<ListingScreen> createState() => _ListingScreenState();
 }
+
+class _ListingScreenState extends State<ListingScreen>{
+    //list for devices
+    List<Map<String, dynamic>> _items = [];
+
+    @override
+    Widget build(BuildContext context){
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Lendme - Devices'),
+            ),
+            body: _items.isEmpty
+                ? const Center(child:Text('No devices added yet'))
+                : ListView.builder(
+                    itemCount: _items.length,
+                    itemBuilder: (context, index){
+                        final item = _items[index];
+                        return ListTile(
+                            leading: const Icon(Icons.devices),
+                            title: Text(item['title']),
+                            subtitle: Text('€${item['price']} / day'),
+                        );
+                    },
+                ),
+            floatingActionButton: FloatingActionButton(
+                onPressed: (){
+                    //adding items
+
+                    setState((){
+                        _items.add({
+                            'title': 'Stofzuiger Dyson',
+                            'price': 5,
+                        });
+                    });
+                },
+                child: const Icon(Icons.add),
+            ),
+
+        );
+    }
+ }
